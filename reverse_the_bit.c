0919330c9ade2ed4b0d0016bfc5b405c23732e8f
Harshal100kar <132765368+Harshal100kar@users.noreply.github.com>
2023-07-13 22:36:11 +0530

    Add files via upload

---
+++ reverse_the_bit.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main()
+{
+	int num,i,rnum=0;
+
+	printf("Enter the number:");
+	scanf("%d",&num);
+
+	for(i=0;i<32;i++)
+	{
+		if(i%8 == 0)
+			printf(" ");
+
+		printf("%d",num>>(31-i) & 0x01);
+	}
+	printf("\n");
+
+	for(i=0;i<32;i++)
+	{
+		if(num & (1<<i))
+			rnum = rnum | (1<<(31-i));
+
+	}
+
+	for(i=0;i<32;i++)
+	{
+		if(i%8 == 0)
+			printf(" ");
+
+		printf("%d",rnum>>(31-i) & 0x01);
+	}
+	printf("\n");
+
+	return 0;
+
+}
